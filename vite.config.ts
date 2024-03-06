17af47b0f44bdcb8e4c43a193e0b8237e7e5e64c
Yrobot <Yrobot@qq.com>
2024-03-06 20:30:56 +0800

    support build web components

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,19 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        "my-badge": "src/components/my-badge.wc.svelte",
+        "my-button": "src/components/my-button.wc.svelte",
+      },
+      formats: ["es"],
+      fileName: (format, entryName) => `${entryName}.js`,
+    },
+  },
   plugins: [
     svelte({
       preprocess: vitePreprocess(),
-      include: /\.wc\.svelte$/ as any,
       compilerOptions: {
         customElement: true,
       },
